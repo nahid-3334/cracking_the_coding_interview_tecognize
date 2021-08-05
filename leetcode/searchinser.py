ebd133cbcfba6fc922d82a23a9a3ac68a89a1471
Md. Nahidul Islam <nahidulss@gmail.com>
2021-08-05 21:39:37 +0600

    binary serach(log(n)) serachinsert added

--- leetcode/searchinser.py
+++ leetcode/searchinser.py
@@ -1,16 +1,24 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        end=len(nums)
-      
-        for i in range(end):
-            if nums[i]==target:# match return
-                return i
-            elif nums[i]>target:
-                return  i
-            elif i== end-1 :
-                 return end
+        left,right=0,len(nums)-1
+        while (left <= right) :
+            if target<nums[left] :#target <initial
+                return left
+            elif target>nums[right] :#target >end
+                 return right+1  
+            else:
+                mid= int(left + (right - left) / 2)
+            #Check if target is present at mid
+                if (nums[mid] == target):
+                    return mid
+            #If target greater, ignore left half
+                elif nums[mid] < target:
+                     left = mid + 1
+            #If target is smaller, ignore right half
+                else:
+                    right = mid - 1;
 # Complexity analysis
 
-# Time complextiy : O(n). 
+# Time complextiy : O(logn). 
 
 # Space complexity : O(1)
